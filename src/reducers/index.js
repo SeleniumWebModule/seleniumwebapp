ac2714ea065a145ba0e73f2dff9d56e4b90693b2
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-07-14 13:35:33 -0300

    Remoção de código estático e inclusão de tratamento para manter os dados via cookie para manter os dados locais antes de sincronizar com o banco.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { SELECTED_PATH } from '../constants';
 import { REGISTER_SCREEN } from '../constants';
 import { REGISTER_SYSTEM } from '../constants';
 
+import { bake_cookie, read_cookie } from 'sfcookies';
+
 const selectedPath = (action) => {
   return {
     currentPath: action.currentPath
@@ -27,45 +29,20 @@ let stateReducer = {
 }
 
 const states = (state=[], action) => {
-  let currentScreen = '';
-  let screens = [];
-  let systems = [];
-
+  stateReducer = read_cookie('stateReducer')
+  
   switch(action.type) {
     case SELECTED_PATH:
-
-       stateReducer = {
-          currentPath: selectedPath(action).currentPath,
-          screens:screens,
-          systems:systems
-        }
-
+        stateReducer.currentPath = selectedPath(action).currentPath;
+        bake_cookie('stateReducer', stateReducer);
       return stateReducer;
     case REGISTER_SCREEN:
-
-      currentScreen = stateReducer.currentPath;
-      systems = stateReducer.systems;
-      screens = stateReducer.screens;
-      screens.push(registerScreen(action).newScreen);
-      
-      stateReducer = {
-        currentPath: currentScreen,
-        systems: systems,
-        screens: screens
-      }
-
+      stateReducer.screens.push(registerScreen(action).newScreen);
+      bake_cookie('stateReducer', stateReducer);
       return stateReducer;
     case REGISTER_SYSTEM:
-      currentScreen = stateReducer.currentPath;
-      systems = stateReducer.systems;
-      systems.push(registerSystem(action).newSystem);
-
-      stateReducer = {
-        currentPath: currentScreen,
-        systems,
-        screens
-      }
-
+      stateReducer.systems.push(registerSystem(action).newSystem);
+      bake_cookie('stateReducer', stateReducer);
       return stateReducer;
     default:
       return states;
