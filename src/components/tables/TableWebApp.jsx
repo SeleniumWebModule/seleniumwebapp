90eb3b6bdf457945d7c544f6fc2a82f1c00c6fde
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-07-15 16:29:21 -0300

    Remoção de texto estático, remoção de tratamento via css

--- src/components/tables/TableWebApp.jsx
+++ src/components/tables/TableWebApp.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Subheader from 'material-ui/Subheader';
-import TableSystem from './TableSystem';
 
 class TableWebApp extends React.Component {
   render(){
-    const table = (
-      <div style={this.props.table === 'system' ? {display: ''} : {display: 'none'}}>
-        <Subheader>Sistemas</Subheader>
-        <TableSystem tableHeader={this.props.tableHeader} tableValues={this.props.tableValues}/>
+    const tableModel = (
+      <div>
+        <Subheader>{this.props.tableHeader}</Subheader>
+        {this.props.table}
       </div>
     )
 
     return(
-      table
+      tableModel
     );
   }
 }
