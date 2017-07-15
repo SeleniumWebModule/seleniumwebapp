3a3725bb6f65fce6ad37217b20d441fd23814eaa
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-07-15 16:32:11 -0300

    Criação de tabela de eventos

---
+++ src/components/tables/TableEvent.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+export default class TableEvent extends React.Component {
+  render() {
+
+    const tableEvent = (
+
+      <Table
+      selectable={false}
+      fixedHeader={true}
+      headersColumn={this.props.headersColumn}
+      tableHeader="Events"
+      tableValues={this.props.tableValues}
+      >
+        <TableHeader>
+          <TableRow>
+            {this.props.headersColumn.map((row) => (
+              <TableHeaderColumn key={row.toString()}>{row}</TableHeaderColumn>
+            ))}
+          </TableRow>
+
+        </TableHeader>
+        <TableBody
+          displayRowCheckbox={true}
+          showRowHover={true}
+          stripedRows={false}>
+           {this.props.tableValues.map((row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{row.eventName}</TableRowColumn>
+                <TableRowColumn>{row.ruleName}</TableRowColumn>
+              </TableRow>
+            ))}
+          </TableBody>
+      </Table> 
+    )
+
+    return(
+      tableEvent
+    );
+  }
+}
