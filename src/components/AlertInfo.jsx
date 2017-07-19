3026f84ba396fe99b7d8b1c9e7d1cbcddb2c8989
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-07-19 14:34:56 -0300

    Criação do componente para exibição de instruções no comportamento do sistema.

---
+++ src/components/AlertInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+
+export default class AlertInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      style: {display: ''}
+    }
+  }
+
+  hideMessage() {
+    this.setState({
+      style: {display: 'none'}
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Alert bsStyle="info" style={this.state.style} bsSize="small"
+          onClick={(event) => {this.hideAlert.bind(this)}}>
+          <strong>Instruções</strong><br /> {this.props.messageBody}
+        </Alert>
+      </div>
+    );
+  }
+}
